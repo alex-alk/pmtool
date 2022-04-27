747e75f7bd09d3e3b931e194557ce9bcf13f7137
Alex <alexandruleonte06@yahoo.com>
2022-04-27 21:55:22 +0300

    added crud

--- src/main/java/com/alexandruleonte/pmtool/services/ProjectService.java
+++ src/main/java/com/alexandruleonte/pmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.alexandruleonte.pmtool.services;
 
 import com.alexandruleonte.pmtool.domain.Project;
+import com.alexandruleonte.pmtool.exceptions.ProjectIdException;
 import com.alexandruleonte.pmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,32 @@ public class ProjectService {
     ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+    }
+
+    public Project findProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exist");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exist");
+        }
+
+        projectRepository.delete(project);
     }
 }
